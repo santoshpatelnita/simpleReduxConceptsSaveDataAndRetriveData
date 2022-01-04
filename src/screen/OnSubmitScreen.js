09dc07b742fc314bb4d18d074bfeeda15674b4e2
Edunext <santoshpatel.edunext@gmail.com>
2022-01-04 17:01:40 +0530

    Add another screen

---
+++ src/screen/OnSubmitScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const OnSubmitScreen = () => {
+    return (
+        <View>
+            <Text>On Submit Button</Text>
+        </View>
+    )
+}
+
+export default OnSubmitScreen
